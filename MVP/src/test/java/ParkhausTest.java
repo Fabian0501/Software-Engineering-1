3a276654cfb6d983ef53cabac8370aebfef3e416
hto2s <huy.to@smail.inf.h-brs.de>
2023-04-18 19:50:48 +0200

    Tests hinzugefügt

--- MVP/src/test/java/ParkhausTest.java
+++ MVP/src/test/java/ParkhausTest.java
@@ -1,2 +1,65 @@
+import org.junit.jupiter.api.Test;
+
 public class ParkhausTest {
+
+    @Test
+    public void ticketZiehenTest(){
+        //Arrange
+
+        //Act
+        Parkticket actual = ticketZiehen(); // Methode Ticketziehen gibt ein Objekt "Parkticket" zurück
+
+        //Assert
+        assertEquals(true,actual.ticketWurdeGezogen()); // Parkticket hat ein boolean-Attribut, ob es gezogen wurde
+    }
+
+    @Test
+    public void schrankeEingangTest(){
+        //Arrange
+        Parkticket ticket = new Parkticket();
+        //Act
+        ticket.schranke(); // Methode schranke() setzt das Attribut "belegt" von Parkticket auf "true" beim Reinfahren
+        //Arrange
+        assertEquals(true,ticket.getTicketBelegt()); // prüfe das Attribut "belegt" auf "true"
+
+    }
+
+    @Test
+    void bezahlen(){
+        //Arrange
+        Parkticket ticket = new Parkticket();
+
+        //Act
+        a.bezahlen(); // Makiert das Ticket als wurde bezahlt
+
+        //Assert
+        assertEquals(true,a.getTicketStatus()); // Prüfe,ob das Ticket bezahlt wurde
+
+    }
+
+
+    @Test
+    public void schrankeAusgangTest(){
+        //Arrange
+        Parkticket ticket = new Parkticket();
+
+        //Act
+       ticket.schranke(); /* Methode schranke() setzt das Attribut "belegt" von Parkticket auf "false" beim Rausfahren,
+         setze Attribut "belegt" nur auf false, wenn Ticket bezahlt wurde sonst nicht*/
+
+        //Arrange
+        assertEquals(false,ticket.getTicketBelegt());// prüfe das Attribut "belegt" auf "false"
+
+
+    }
+
+
+
+
+
+
+
+
+
+
 }
